d79f1f70ad69d9258f4c2edaee40845a79d3d8dd
softwaiter <softwaiter@126.com>
2021-03-20 13:06:03 +0800

    增加代理热加载接口

---
+++ Controllers/ProxyReload.cs
@@ -0,0 +1,25 @@
+﻿using CodeM.FastApi.Context;
+using CodeM.FastApi.Logger;
+using CodeM.FastApi.System;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeM.FastApi.Controllers
+{
+    public class ProxyReload
+    {
+        public async Task Handle(ControllerContext cc)
+        {
+            try
+            {
+                ProxyUtils.Reload();
+                await cc.JsonAsync("成功！");
+            }
+            catch (Exception exp)
+            {
+                LogUtils.Error(exp);
+                await cc.JsonAsync("代理重新加载失败。");
+            }
+        }
+    }
+}
