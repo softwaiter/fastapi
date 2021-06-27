ff3c2e5e6c66a87a027de341f0e284248c429d23
softwaiter <softwaiter@126.com>
2021-06-27 20:05:09 +0800

    :construction:增加产品购买价格预估接口

---
+++ Controllers/CalcPrice.cs
@@ -0,0 +1,30 @@
+﻿using CodeM.FastApi.Context;
+using System.Threading.Tasks;
+
+namespace CodeM.FastApi.Controllers
+{
+    public class CalcPrice
+    {
+
+        public async Task Handle(ControllerContext cc)
+        {
+            string prod = cc.QueryParams.Get("prod", null);
+            string month = cc.QueryParams.Get("month", null);
+
+            if (string.IsNullOrWhiteSpace(prod) ||
+                string.IsNullOrWhiteSpace(month))
+            {
+                await cc.JsonAsync(-1, null, "缺少参数。");
+                return;
+            }
+
+            int monthNum;
+            if (!int.TryParse(month, out monthNum))
+            {
+                await cc.JsonAsync(-1, null, "month参数无效。");
+                return;
+            }
+        }
+
+    }
+}
