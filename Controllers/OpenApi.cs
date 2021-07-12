eabd60f414ebd3305f775163b9a0fa6645c032b0
softwaiter <softwaiter@126.com>
2021-07-12 18:43:58 +0800

    :construction:增加用户密钥管理服务

---
+++ Controllers/OpenApi.cs
@@ -0,0 +1,109 @@
+﻿using CodeM.FastApi.Context;
+using CodeM.FastApi.Services;
+using System.Threading.Tasks;
+
+namespace CodeM.FastApi.Controllers
+{
+    public class OpenApi
+    {
+        public async Task Query(ControllerContext cc)
+        {
+            string userid = cc.RouteParams["userid"];
+            if (string.IsNullOrWhiteSpace(userid))
+            {
+                await cc.JsonAsync(-1, null, "非法的路由参数。");
+                return;
+            }
+
+            dynamic userKey = UserKeyService.GetOpenApiByUserId(userid);
+            if (userKey != null)
+            {
+                await cc.JsonAsync(new
+                {
+                    OpenId = userKey.OpenId,
+                    PublicKey = userKey.PublicKey,
+                    Actived = userKey.Actived
+                });
+            }
+            else
+            {
+                await cc.JsonAsync(null);
+            }
+        }
+
+        public async Task Create(ControllerContext cc)
+        {
+            string userid = cc.RouteParams["userid"];
+            if (string.IsNullOrWhiteSpace(userid))
+            {
+                await cc.JsonAsync(-1, null, "非法的路由参数。");
+                return;
+            }
+
+            dynamic userKey = UserKeyService.GetOpenApiByUserId(userid);
+            if (userKey == null)
+            {
+                userKey = UserKeyService.NewOpenApi(userid);
+            }
+
+            await cc.JsonAsync(new
+            {
+                OpenId = userKey.OpenId,
+                PublicKey = userKey.PublicKey,
+                Actived = userKey.Actived
+            });
+        }
+
+        public async Task Replace(ControllerContext cc)
+        {
+            string userid = cc.RouteParams["userid"];
+            if (string.IsNullOrWhiteSpace(userid))
+            {
+                await cc.JsonAsync(-1, null, "非法的路由参数。");
+                return;
+            }
+
+            dynamic userKey = UserKeyService.ReplaceSecret(userid);
+            if (userKey != null)
+            {
+                await cc.JsonAsync(new
+                {
+                    OpenId = userKey.OpenId,
+                    PublicKey = userKey.PublicKey,
+                    Actived = userKey.Actived
+                });
+            }
+            else
+            {
+                await cc.JsonAsync(-1, null, "请先创建密钥。");
+            }
+        }
+
+        public async Task Enable(ControllerContext cc)
+        {
+            string userid = cc.RouteParams["userid"];
+            if (string.IsNullOrWhiteSpace(userid))
+            {
+                await cc.JsonAsync(-1, null, "非法的路由参数。");
+                return;
+            }
+
+            if (cc.PostJson == null ||
+                cc.PostJson.enable == null)
+            {
+                await cc.JsonAsync(-1, null, "无效的参数。");
+                return;
+            }
+
+            bool bRet = UserKeyService.Enable(userid, cc.PostJson.enable);
+            if (bRet)
+            {
+                await cc.JsonAsync("SUCC");
+            }
+            else
+            {
+                await cc.JsonAsync(-1, null, "请先创建密钥。");
+            }
+        }
+    }
+}
---
+++ Services/UserKeyService.cs
@@ -0,0 +1,71 @@
+﻿using CodeM.Common.Orm;
+using System;
+using System.Collections.Generic;
+using XC.RSAUtil;
+
+namespace CodeM.FastApi.Services
+{
+    public class UserKeyService
+    {
+        public static dynamic GetOpenApiByUserId(string userid)
+        {
+            dynamic user = UserService.GetUserById(userid);
+            if (user != null)
+            {
+                dynamic result = OrmUtils.Model("UserKey").Equals("User", user.Code).QueryFirst();
+                return result;
+            }
+            return null;
+        }
+
+        public static dynamic NewOpenApi(string userid)
+        {
+            dynamic user = UserService.GetUserById(userid);
+            if (user != null)
+            {
+                List<string> result = RsaKeyGenerator.Pkcs1Key(2048, true);
+
+                string[] prefixItems = new string[] { "6", "8", "9" };
+                string timeStr = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                string openid = prefixItems[new Random().Next(3)] + timeStr.Substring(1);
+
+                dynamic newUserKey = OrmUtils.Model("UserKey").NewObject();
+                newUserKey.User = user.Code;
+                newUserKey.OpenId = openid;
+                newUserKey.PrivateKey = result[0];
+                newUserKey.PublicKey = result[1];
+
+                OrmUtils.Model("UserKey").SetValues(newUserKey).Save();
+
+                return newUserKey;
+            }
+            return null;
+        }
+
+        public static dynamic ReplaceSecret(string userid)
+        {
+            dynamic userKey = GetOpenApiByUserId(userid);
+            if (userKey != null)
+            {
+                List<string> result = RsaKeyGenerator.Pkcs1Key(2048, true);
+                userKey.PrivateKey = result[0];
+                userKey.PublicKey = result[1];
+                OrmUtils.Model("UserKey").SetValues(userKey).Equals("Id", userKey.Id).Update();
+                return userKey;
+            }
+            return null;
+        }
+
+        public static bool Enable(string userid, bool enable)
+        {
+            dynamic userKey = GetOpenApiByUserId(userid);
+            if (userKey != null)
+            {
+                userKey.Actived = enable;
+                OrmUtils.Model("UserKey").SetValues(userKey).Equals("Id", userKey.Id).Update();
+                return true;
+            }
+            return false;
+        }
+    }
+}
